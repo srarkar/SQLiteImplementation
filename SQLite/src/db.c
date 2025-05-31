9203d4ba9d667a7bd7b38efc6be198ceb905c409
srarkar <rickdsarkar@gmail.com>
2025-05-31 17:59:59 -0400

    table struct and some consts

--- SQLite/src/db.c
+++ SQLite/src/db.c
@@ -3,15 +3,6 @@
 #include <string.h>
 #include "../include/db.h"
 
-// compact representation of a table row
-const uint32_t ID_SIZE = size_of_attribute(Row, id);
-const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
-const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-const uint32_t ID_OFFSET = 0;
-const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
-
 /*
 layout of serialized row:
 
@@ -119,8 +110,6 @@ void execute_statement(Statement* statement) {
       break;
   }
 }
-
-
 
 
 // basic REPL
